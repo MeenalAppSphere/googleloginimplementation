a60085fce08dcac397edb406530ce9cbd9dbd68c
MeenalAppSphere <meenal.solanki@appsphere.in>
2020-08-06 15:04:17 +0530

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,16 +12,17 @@ import { HelloComponent } from './hello.component';
 import { LoginComponent } from './login/login.component';
 import {LoginService} from './login/login.service';
 import { ListComponent } from './list/list.component';
+import { AuthGuard } from './login/auth.guard';
 
 const route: Routes=[
   {path:"",component:LoginComponent},
-  {path:"list",component:ListComponent}
+  {path:"list",component:ListComponent,canActivate:[AuthGuard]}
 ]
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AngularFireAuthModule,AngularFireDatabaseModule,AngularFireModule.initializeApp(environment.firebaseConfig),RouterModule.forRoot(route) ],
+  imports: [ BrowserModule, FormsModule, AngularFireAuthModule,AngularFireDatabaseModule,AngularFireModule.initializeApp(environment.firebaseConfig),RouterModule.forRoot(route) ],
   declarations: [ AppComponent, HelloComponent, LoginComponent, ListComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [LoginService],
+  providers: [LoginService ,AuthGuard],
 })
 export class AppModule { }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import * as firebase from "firebase/app";
 
 @Component({
   selector: 'app-list',
@@ -6,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  name:string;
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+    var user = firebase.auth().currentUser;
+var name, email, photoUrl, uid, emailVerified;
+
+if (user != null) {
+ this. name = user.displayName;
+  email = user.email;
+  photoUrl = user.photoURL;
+  emailVerified = user.emailVerified;
+  uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+                   // this value to authenticate with your backend server, if
+                   // you have one. Use User.getToken() instead.
+}
+  }
+
+  logout()
+  {
+      this.router.navigate([''])
   }
 
 }
